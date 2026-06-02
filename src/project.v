7ee10b578bfdb1110d814f8e1c8644c00ed3f991
Dinesh-vlsi <akdinesh2004@gmail.com>
2026-06-02 11:58:36 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,12 +1,24 @@
 `default_nettype none
 
 module tt_um_decoder (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out    // Dedicated outputs
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+
+    input  wire ena,
+    input  wire clk,
+    input  wire rst_n
 );
 
-    // Simple 8-bit pass-through decoder
+    // Pass input directly to output
     assign uo_out = ui_in;
+
+    // Unused bidirectional pins
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
 
 endmodule
 
