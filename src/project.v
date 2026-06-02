9aa2a90344830d277552800fc0d0dcea7b616cd9
Dinesh-vlsi <akdinesh2004@gmail.com>
2026-06-02 14:33:47 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,18 +1,17 @@
 module tt_um_decoder (
     input  [7:0] ui_in,
     output [7:0] uo_out,
-    ...
+    input  [7:0] uio_in,
+    output [7:0] uio_out,
+    output [7:0] uio_oe,
+    input        ena,
+    input        clk,
+    input        rst_n
 );
 
-    wire _unused = &{ena, clk, rst_n, uio_in};
+assign uo_out = ui_in + 8'd30;  // 20 + 30 = 50
 
-    // Pass-through
-    assign uo_out = ui_in;
-
-    // Disable bidirectional pins
-    assign uio_out = 8'h00;
-    assign uio_oe  = 8'h00;
+assign uio_out = 8'b0;
+assign uio_oe  = 8'b0;
 
 endmodule
-
-`default_nettype wire
